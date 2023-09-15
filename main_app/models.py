3bd9d3d81141bcfda07af0f21da1c84efd8ebaf9
benjaehyun <139726967+benjaehyun@users.noreply.github.com>
2023-09-15 15:59:04 -0700

    added photo and subforum class

--- main_app/models.py
+++ main_app/models.py
@@ -1,8 +1,26 @@
 from django.db import models
 from django.urls import reverse
-from datetime import date 
+from datetime import date, datetime
 from django.contrib.auth.models import User 
 # Create your models here.
 
 class Post(models.Model): 
-    content = models.TextField(max_length=)
+    title = models.CharField(max_length=100)
+    content = models.TextField(max_length=600)
+    date = models.DateField(auto_now_add=True)
+  
+    def __str__(self):
+        return f'Photo for post_id: {self.post_id} @{self.url}'
+
+
+class Photo(models.Model): 
+    url = models.CharField(max_length=200)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'Photo for post_id: {self.post_id} @{self.url}'
+
+class Subforum(models.Model): 
+    title = models.CharField(max_length=100)
+    date = models.DateField(auto_now_add=True)
+    likes = models.IntegerField(default=0)
--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,37 @@
-from django.shortcuts import render
+import uuid
+import boto3
+import os
+from django.shortcuts import render, redirect
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+# from .forms import FeedingForm
+from .models import Post, Photo
+
 
 # Create your views here.
+def home(request): 
+    subforums = S
+    return render(request, home.html)
+
+def add_photo(request, photo_id):
+    photo_file = request.FILES.get('photo-file', None)
+    if photo_file:
+        s3 = boto3.client('s3')
+        # need a unique "key" for S3 / needs image file extension too
+        key = uuid.uuid4().hex[:6] + photo_file.name[photo_file.name.rfind('.'):]
+        # just in case something goes wrong
+        try:
+            bucket = os.environ['S3_BUCKET']
+            s3.upload_fileobj(photo_file, bucket, key)
+            # build the full url string
+            url = f"{os.environ['S3_BASE_URL']}{bucket}/{key}"
+            # we can assign to cat_id or cat (if you have a cat object)
+            Photo.objects.create(url=url, photo_id=photo_id)
+        except Exception as e:
+            print('An error occurred uploading file to S3')
+            print(e)
+    return redirect('detail', photo_id=photo_id)
+
+def create_post(request): 
+    Post.object.create()
+    id = post_id 
+    Photo.objects.create(url=url, post_id = id)
