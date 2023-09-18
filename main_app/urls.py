b771c0ff2f54bf4337aa3594ddfcda1e149e89ba
nadewd <nadewbdemissew@gmail.com>
2023-09-18 14:22:56 -0700

    Added update functionality to Subform and Post data entities

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,9 +4,11 @@ from . import views
 urlpatterns = [
 	path('', views.home, name='home'),
 	path('about/', views.about, name='about'),
-	path('subforums/<int:subforum_id>/', views.subforums_detail, name='subforums_detail'),
 	path('subforums/create/', views.SubforumCreate.as_view(), name='subforums_create'),
+	path('subforums/<int:subforum_id>/', views.subforums_detail, name='subforums_detail'),
 	path('subforums/<int:subforum_id>/add_post/', views.add_post, name='add_post'),
+	path('subforums/<int:subforum_id>/update_post', views.update_post, name='update_post'),
+	path('subforums/<int:subforum_id>/delete_post', views.delete_post, name='delete_post'),
 	path('companies/', views.CompanyList.as_view(), name='company_index'),
 	path('companies/<int:pk>/', views.CompanyDetail.as_view(), name='company_detail'),
 	path('companies/create/', views.CompanyCreate.as_view(), name='company_create'),
--- main_app/views.py
+++ main_app/views.py
@@ -9,22 +9,21 @@ from django.views.generic import ListView, DetailView
 from .models import Subforum, Post, Company
 from .forms import PostForm
 
-def subforms_index(request): 
+# Meta View Functions
+def home(request): 
     subforums = Subforum.objects.all()
     # Like this, or can redirect to subforum/index.html instead
     return render(request, 'home.html', { 
         'subforums': subforums
     })
 
-def company_subforms_index(request):
-    company_subforums = Company_Subforum.objects.all()
-    # Where should this live? I picked this one because this file already exists
-    return render(request, 'subforum/index.html', {
-        'company_subforums': company_subforums
-    }) 
-
 def about(request): 
     return render(request, 'about.html')
+
+# Subforum CRUD views
+class SubforumCreate(CreateView): 
+    model = Subforum
+    fields = ['title', 'pinned']
 
 def subforums_detail(request, subforum_id): 
     subforum = Subforum.objects.get(id=subforum_id)
@@ -34,29 +33,45 @@ def subforums_detail(request, subforum_id):
         'post_form': post_form
         })
 
-def company_subforums_detail(request, company_subforum_id): 
-    company_subforum = Company_Subforum.objects.get(id=company_subforum_id)
-    post_form = PostForm()
-    return render(request, 'comapny_subforum/detail.html', {
-        'company_subforum': company_subforum, 
-        'post_form': post_form
-        })
-
-class SubforumCreate(CreateView): 
+class SubforumUpdate(UpdateView):
     model = Subforum
-    fields = ['title', 'pinned']
+    fields = ['age', 'team', 'description']
 
+
+
+# Post CRUD views
+def add_post(request, subforum_id): 
+    form = PostForm(request.POST)
+    if form.is_valid():
+        new_post = form.save(commit = False)
+        new_post.subforum_id = subforum_id
+        new_post.save()
+    return redirect('subforums_detail', subforum_id = subforum_id)
+
+def update_post(request, post_id, subforum_id):
+    post = Post.objects.get(id = post_id)
+    form = PostForm(request.POST)
+    if form.is_valid():
+        post = form.save(commit=False)
+        post.save()
+    return redirect('subforum_detail', subforum_id = subforum_id)
+
+class DeletePost(DeleteView):
+    model = post
+    success_url = '/subforum/<int:subforum_id/'
+
+
+# Company Subforum Views
 class Company_SubforumCreate(CreateView): 
     model = Company_Subforum
     fields = ['title', 'pinned']
 
-def add_post(request, subforum_id): 
-    form = PostForm(request.POST)
-    if form.is_valid():
-        new_post = form.save(commit=False)
-        new_post.subforum_id = subforum_id
-        new_post.save()
-    return redirect('subforums_detail', subforum_id = subforum_id)
+def company_subfoums_index(request):
+    company_subforums = Company_Subforum.objects.all()
+    # Where should this live? I picked this one because this file already exists
+    return render(request, 'company/detail.html', {
+        'company_subforums': company_subforums
+    })
 
 def add_company_post(request, company_subforum_id): 
     form = PostForm(request.POST)
@@ -66,6 +81,19 @@ def add_company_post(request, company_subforum_id):
         new_post.save()
     return redirect('company_subforums_detail', company_subforum_id = company_subforum_id)
 
+def company_subforums_detail(request, company_subforum_id): 
+    company_subforum = Company_Subforum.objects.get(id=company_subforum_id)
+    post_form = PostForm()
+    return render(request, 'comapny_subforum/detail.html', {
+        'company_subforum': company_subforum, 
+        'post_form': post_form
+        })
+
+
+
+
+
+# Company Views
 class CompanyList(ListView): 
     model = Company 
     template_name = "company/index.html"
