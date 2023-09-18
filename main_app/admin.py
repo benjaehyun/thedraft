5360cae5d854ad350257dd5410a576525900223a
benjaehyun <139726967+benjaehyun@users.noreply.github.com>
2023-09-18 11:39:42 -0700

    added new models to admin

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
-from .models import Subforum, Post, Company
+from .models import Subforum, Post, Company, Photo, Company_Subforum, Company_Post
 
 # Register your models here.
 
 admin.site.register(Subforum)
 admin.site.register(Post)
 admin.site.register(Company)
+admin.site.register(Photo)
+admin.site.register(Company_Subforum)
+admin.site.register(Company_Post)
